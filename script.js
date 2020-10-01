1ef362c966e0f479042c28800690af80f515d620
Leannaland <leannaland2011@gmail.com>
2020-10-01 19:34:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+//AJAX Call 
+
+$(document).ready(function() {
+
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8ba7a22485b2dbc12c26d0bdf2609619",
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
+
+
+  
+    // Click Event to pull weather
+
+     $(".button").on("click", function(event) {
+
+      var city = $("#citySearch").val().trim();  
+
+       event.preventDefault();
+  
+      var weatherData = $(this).attr("data-weather");
+      var queryURL = "http://api.openweathermap.org/data/2.5/weather?q= "+ city + "&appid=8ba7a22485b2dbc12c26d0bdf2609619"
+      //http://api.openweathermap.org/data/2.5/weather?q= + city name + &appid={API key} "8ba7a22485b2dbc12c26d0bdf2609619"
+      
+       
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+       console.log(response)
+
+      var cityName = response.name;
+      console.log(cityName)
+       
+     
+    })
+  
+   
+     })
+
+    })
